e06e821c15cb7e0ba2a415fc580856a77616bd56
glkurkin <166190897+glkurkin@users.noreply.github.com>
2024-09-07 00:48:30 +0800

    Update EpicStatusTest.java

--- test/EpicStatusTest.java
+++ test/EpicStatusTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
-import java.time.Duration;
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EpicStatusTest {
 
