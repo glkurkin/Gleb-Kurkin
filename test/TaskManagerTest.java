151ac67570a11df259fb60ff57a61270c85ca3fe
glkurkin <166190897+glkurkin@users.noreply.github.com>
2024-09-07 00:47:57 +0800

    Update TaskManagerTest.java

--- test/TaskManagerTest.java
+++ test/TaskManagerTest.java
@@ -2,7 +2,10 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public abstract class TaskManagerTest<T extends TaskManager> {
 
