f44f4cf542502312e44d604daedbc40ab163197a
glkurkin <166190897+glkurkin@users.noreply.github.com>
2024-09-07 00:44:41 +0800

    Update InMemoryTaskManagerTest.java

--- test/InMemoryTaskManagerTest.java
+++ test/InMemoryTaskManagerTest.java
@@ -1,14 +1,17 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
-public class InMemoryTaskManagerTest {
+
+public class InMemoryTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
     private TaskManager taskManager = Managers.getDefault();
 
     @Test
     public void testCreateAndRetrieveTask() {
-        Task task = taskManager.createTask("Задача 1", "Описание задачи 1");
+        Task task = taskManager.createTask("Задача 1", "Описание задачи 1", Duration.ofMinutes(30), LocalDateTime.now());
         Assertions.assertNotNull(task);
         Task retrievedTask = taskManager.getTaskById(task.getId());
         Assertions.assertEquals(task, retrievedTask);
@@ -33,8 +36,8 @@ public class InMemoryTaskManagerTest {
 
     @Test
     public void testTaskHistory() {
-        Task task1 = taskManager.createTask("Задача 1", "Описание задачи 1");
-        Task task2 = taskManager.createTask("Задача 2", "Описание задачи 2");
+        Task task1 = taskManager.createTask("Задача 1", "Описание задачи 1", Duration.ofMinutes(30), LocalDateTime.now());
+        Task task2 = taskManager.createTask("Задача 2", "Описание задачи 2", Duration.ofMinutes(30), LocalDateTime.now());
         taskManager.getTaskById(task1.getId());
         taskManager.getTaskById(task2.getId());
 
@@ -46,15 +49,15 @@ public class InMemoryTaskManagerTest {
 
     @Test
     public void testDeleteTask() {
-        Task task = taskManager.createTask("Задача 1", "Описание задачи 1");
+        Task task = taskManager.createTask("Задача 1", "Описание задачи 1", Duration.ofMinutes(30), LocalDateTime.now());
         taskManager.deleteTask(task.getId());
         Assertions.assertNull(taskManager.getTaskById(task.getId()));
     }
 
     @Test
     public void testGetHistory() {
-        Task task1 = taskManager.createTask("Задача 1", "Описание задачи 1");
-        Task task2 = taskManager.createTask("Задача 2", "Описание задачи 2");
+        Task task1 = taskManager.createTask("Задача 1", "Описание задачи 1", Duration.ofMinutes(30), LocalDateTime.now());
+        Task task2 = taskManager.createTask("Задача 2", "Описание задачи 2", Duration.ofMinutes(30), LocalDateTime.now());
 
         taskManager.getTaskById(task1.getId());
         taskManager.getTaskById(task2.getId());
@@ -64,4 +67,5 @@ public class InMemoryTaskManagerTest {
         Assertions.assertEquals(task1, history.get(0));
         Assertions.assertEquals(task2, history.get(1));
     }
+
 }
