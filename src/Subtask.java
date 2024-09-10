12a47a50649a15486c334006b6232d5e98deaa4e
glkurkin <166190897+glkurkin@users.noreply.github.com>
2024-09-10 11:58:10 +0800

    Update Subtask.java

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,13 +1,20 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Subtask extends Task {
     private int epicId;
 
-    public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
-        super(id, name, description, status);
+    public Subtask(int id, String name, String description, TaskStatus status, int epicId, Duration duration, LocalDateTime startTime) {
+        super(id, name, description, status, duration, startTime);
         this.epicId = epicId;
     }
 
     public int getEpicId() {
         return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
     }
 
     @Override
