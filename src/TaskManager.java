2ea9a760085afc858b726426e062e5fba080943e
glkurkin <166190897+glkurkin@users.noreply.github.com>
2024-09-10 11:53:51 +0800

    Update TaskManager.java

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,13 +1,15 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TaskManager {
-    Task createTask(String name, String description);
+    Task createTask(String name, String description, Duration duration, LocalDateTime now);
 
     Epic createEpic(String name, String description);
 
     Subtask createSubtask(String name, String description, int epicId);
 
-    Task updateTask(int id, String name, String description);
+    Task updateTask(int id, String name, String description, TaskStatus done);
 
     boolean deleteTask(int id);
 
