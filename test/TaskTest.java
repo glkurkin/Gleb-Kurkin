f9da12784eb59e3e84ebe83f3b23c134d47b6529
glkurkin <166190897+glkurkin@users.noreply.github.com>
2024-09-10 11:52:53 +0800

    Update TaskTest.java

--- test/TaskTest.java
+++ test/TaskTest.java
@@ -1,19 +1,26 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class TaskTest {
 
     @Test
     public void testTaskEquality() {
-        Task task1 = new Task(1, "Задача 1", "Описание задачи 1", TaskStatus.NEW);
-        Task task2 = new Task(1, "Задача 2", "Описание задачи 2", TaskStatus.NEW);
+        Task task1 = new Task(1, "Задача 1", "Описание задачи 1", TaskStatus.NEW, Duration.ofMinutes(60), LocalDateTime.now().plusMinutes(15));
+        Task task2 = new Task(1, "Задача 2", "Описание задачи 2", TaskStatus.NEW, Duration.ofMinutes(60), LocalDateTime.now().plusMinutes(15));
         Assertions.assertEquals(task1, task2);
     }
 
     @Test
     public void testSubtaskEquality() {
-        Subtask subtask1 = new Subtask(1, "Подзадача 1", "Описание подзадачи 1", TaskStatus.NEW, 1);
-        Subtask subtask2 = new Subtask(1, "Подзадача 2", "Описание подзадачи 2", TaskStatus.NEW, 1);
+
+        Duration duration = Duration.ofMinutes(60);
+        LocalDateTime startTime = LocalDateTime.now().plusMinutes(15);
+
+        Subtask subtask1 = new Subtask(1, "Подзадача 1", "Описание подзадачи 1", TaskStatus.NEW, 1, duration, startTime);
+        Subtask subtask2 = new Subtask(1, "Подзадача 2", "Описание подзадачи 2", TaskStatus.NEW, 1, duration, startTime);
         Assertions.assertEquals(subtask1, subtask2);
     }
 }
