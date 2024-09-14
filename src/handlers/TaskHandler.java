bcf01b8ae768f3456f3f8e6be4f40ed25c22a1d4
Gl.Kurkin <gleee.kurk@gmail.com>
2024-09-14 21:00:57 +0800

    Редактирование TaskHandler для CheckStyle

--- src/handlers/TaskHandler.java
+++ src/handlers/TaskHandler.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
